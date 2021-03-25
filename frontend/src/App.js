a95a448b084d645cd45642f02ca4c62d6bc0bebb
zcestkc <kwan9545@gmail.com>
2021-03-25 22:04:23 +0000

    Product and review components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import Product from './components/Product';
 import data from './data';
 
 function App() {
@@ -5,7 +7,9 @@ function App() {
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="/">roe</a>
+          <a className="brand" href="/">
+            roe
+          </a>
         </div>
         <div>
           <a href="/cart">Cart</a>
@@ -13,33 +17,13 @@ function App() {
         </div>
       </header>
       <main>
-        <div className="row center">
-          {data.products.map(product => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img 
-                  className="medium" 
-                  src={product.image} 
-                  alt={product.name} 
-                />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                  <span><i className="fa fa-star"></i></span>
-                </div>
-                <div className="price">${product.price}</div>
-              </div>
-            </div>
-            ))
-          }
-        </div>
+        <div>
+          <div className="row center">
+            {data.products.map(product => (
+                <Product key={product._id} product={product} />
+              ))}
+          </div>
+          </div>
       </main>
       <footer className="row center">All right reserved</footer>
     </div>
