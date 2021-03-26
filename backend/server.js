d06b71d49011629cade5d4e7220f26465b782432
zcestkc <kwan9545@gmail.com>
2021-03-26 11:23:45 +0000

    loading and message boxes

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
-app.listen(5000, () => {
-  console.log('Serve at http://localhost:5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
