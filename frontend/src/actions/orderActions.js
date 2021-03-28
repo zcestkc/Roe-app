22810db03984151e47ae3c60c7382677269dba34
zcestkc <kwan9545@gmail.com>
2021-03-28 20:24:44 +0100

    implemented place order action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,7 +3,6 @@ import { CART_EMPTY } from '../constants/cartConstants';
 import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
-  ORDER_CREATE_RESET,
   ORDER_CREATE_SUCCESS,
 } from '../constants/orderConstants';
 
