a2f15298d8ebe058f6949575face185bbf029df3
zcestkc <kwan9545@gmail.com>
2021-03-29 18:46:29 +0100

    admin view implemented

--- backend/utils.js
+++ backend/utils.js
@@ -35,3 +35,11 @@ export const isAuth = (req, res, next) => {
     res.status(401).send({ message: 'No token' });
   }
 };
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: 'Invald Admin Token' });
+  }
+};
