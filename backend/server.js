3a1db5ef889bba23cefe16a3614fa884aa4303c9
zcestkc <kwan9545@gmail.com>
2021-03-29 21:24:57 +0100

    deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
