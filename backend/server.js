638e0519cc42f008e73a9bc3e93d7f3e204dae8b
zcestkc <kwan9545@gmail.com>
2021-03-29 20:57:06 +0100

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -23,19 +23,21 @@ app.use('/api/orders', orderRouter);
 app.use('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
-}
-
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
